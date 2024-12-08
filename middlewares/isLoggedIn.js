843e41e04ae4cedd20f3a95291a0d33299ef2326
afzal hamdulay <afzalhamdulay1@gmail.com>
2024-12-08 23:12:24 +0400

    changes in header and jwt middleware

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -1,20 +1,21 @@
-const jwt = require('jsonwebtoken');
-const userModel = require("../models/user-model")
+    const jwt = require('jsonwebtoken');
+    const userModel = require("../models/user-model")
 
-module.exports = async function(req,res, next) {
-    if(!req.cookies.token) {
-        req.flash("Error", "You need to login first")
-        return res.redirect("/") 
-    }
+    module.exports = async function(req,res, next) {
+        if(!req.cookies.token) {
+            req.flash("Error", "You need to login first")
+            return res.redirect("/") 
+        }
 
-    try {
-        let decoded = jwt.verify(req.cookies.token, process.env.JWT_KEY)
-        let user = await userModel.findOne({email: decoded.email}).select("-password")
-        req.user = user
-        next()
-    } catch (err) {
-        flash("Error", err.message)
-        res.redirect("/")
+        try {
+            let decoded = jwt.verify(req.cookies.token, process.env.JWT_KEY)
+            let user = await userModel.findOne({email: decoded.email}).select("-password")
+            req.user = user
+            res.locals.userFullname = user.fullname;
+            next()
+        } catch (err) {
+            flash("Error", err.message)
+            res.redirect("/")
+        }
+        
     }
-    
-}
